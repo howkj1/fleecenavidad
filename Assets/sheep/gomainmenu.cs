b19974f95ac641eb7e5170186ebe89f54eab4982
howkj1 <howkj1@gmail.com>
2016-01-14 16:54:13 -0600

    first commit

---
+++ Assets/sheep/gomainmenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gomainmenu : MonoBehaviour {
+	private void OnGUI()
+	{
+		// if(
+		// GUI.Button(new Rect ((widthPlacement), (heightPlacement), widthSize, heightSize), "buttonText")){
+		// 		Application.LoadLevel("yourLevelName");
+		// }
+
+		if (GUI.Button (new Rect (Screen.width -150, Screen.height -80, 100, 60), "Menu")) 
+		{
+				Application.LoadLevel ("menu");
+		}
+
+	}
+}
---
+++ Assets/sheep/sheeploader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sheeploader : MonoBehaviour {
+
+	private void OnGUI()
+	{
+		// if(
+		// GUI.Button(new Rect ((widthPlacement), (heightPlacement), widthSize, heightSize), "buttonText")){
+		// 		Application.LoadLevel("yourLevelName");
+		// }
+
+		if (GUI.Button (new Rect (Screen.width * .5f +200, Screen.height * .5f, 100, 60), "Infinity Mode"))
+		{
+			Application.LoadLevel ("level1");
+		}
+
+
+
+		if (GUI.Button (new Rect (Screen.width * .5f +200, Screen.height * .5f +80, 100, 60), "Sky Mode"))
+		{
+			Application.LoadLevel ("level2");
+		}
+
+	}
+}
